e1964a121e7892171c8c9cbb42163f0ecab0aff3
Bhushan <alhatbhushan@gmail.com>
2025-03-21 23:26:57 +0530

    read input and create tokens

---
+++ internal/lexer/lexer.go
@@ -0,0 +1,72 @@
+package lexer
+
+import (
+	"fmt"
+
+	"github.com/rashmod/monkey-go/internal/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readCh()
+
+	return l
+}
+
+func (lexer *Lexer) readCh() {
+	if lexer.readPosition >= len(lexer.input) {
+		lexer.ch = 0
+	} else {
+		lexer.ch = lexer.input[lexer.readPosition]
+	}
+	lexer.position = lexer.readPosition
+	lexer.readPosition++
+}
+
+func (lexer *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch lexer.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, lexer.ch)
+	case '+':
+		tok = newToken(token.PLUS, lexer.ch)
+	case ',':
+		tok = newToken(token.COMMA, lexer.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, lexer.ch)
+	case '(':
+		tok = newToken(token.LPAREN, lexer.ch)
+	case ')':
+		tok = newToken(token.RPAREN, lexer.ch)
+	case '{':
+		tok = newToken(token.LBRACE, lexer.ch)
+	case '}':
+		tok = newToken(token.RBRACE, lexer.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	lexer.readCh()
+
+	return tok
+}
+
+func newToken(toketype token.TokenType, ch byte) token.Token {
+	return token.Token{Type: toketype, Literal: string(ch)}
+}
+
+func (lexer *Lexer) Print() {
+	fmt.Printf("lexer: %s\n", lexer.input)
+	fmt.Printf("position: %d\n", lexer.position)
+	fmt.Printf("readPosition: %d\n", lexer.readPosition)
+	fmt.Printf("ch: %c\n", lexer.ch)
+}
